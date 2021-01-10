f85834bf1a76126c76b1e59ba077d9d554b75582
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 17:12:17 +0000

    add deploy script

---
+++ deploy-to-prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "🎆 deploying to prod 🎆"
+echo ""
+echo ""
+# npm run lint:fix  
+# npm run test
+echo ""
+echo ""
+rm -rf dist/ 
+npm run build 
+git add * 
+git commit -am "deploy"
+git push heroku master
