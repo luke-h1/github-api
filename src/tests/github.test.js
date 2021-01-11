34e40bbc82a14b6b5dde800448453ccdfa768504
luke-h1 <luke.howsam@yahoo.com>
2021-01-11 08:45:04 +0000

    update tests

--- src/tests/github.test.js
+++ src/tests/github.test.js
@@ -6,14 +6,6 @@ test('github routes should return 200', async () => {
   expect(response.statusCode).toBe(200)
 })
 
-test('/api/github/search/users/luke-h1 should have certain keys', async () => {
-  const response = await request(app).get('/api/city/weather/london')
-  expect(response.body).toHaveProperty('data.location')
-  expect(response.body).toHaveProperty('data.current')
-  expect(response.body).toHaveProperty('data.location.region')
-  expect(response.body).toHaveProperty('data.location.name')
-  expect(response.body).toHaveProperty('data.location.region')
-  expect(response.body).toHaveProperty('data.current.condition')
-  expect(response.body).toHaveProperty('data.current.wind_mph')
-  expect(response.body).toHaveProperty('data.current.feelslike_c')
-})
+// test('/api/github/search/users/luke-h1 should have certain keys', async () => {
+//   const response = await request(app).get('/api/github/search/users/luke-h1')
+// })
